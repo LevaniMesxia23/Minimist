246614f6a3c45fd61ae962bf02aac0026242b4fb
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2025-03-08 20:59:52 +0400

    added path and fs to get text from file

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
+import path from "path";
+import fs from "fs";
 import minimist from "minimist";
+
+
 const args = minimist(process.argv.slice(2), {
   boolean: ["help"],
   string: ["file"],
@@ -7,17 +11,32 @@ const args = minimist(process.argv.slice(2), {
 
 if (args.help) {
   printHelp();
-} else {
+} else if (args.file) {
+  fs.readFile(path.resolve(args.file), "utf-8", function onContents(err, contents) {
+    if(err){
+      error(err.toString(), true)
+    } else {
+      processFile(contents)
+    }
+  })
+}
+ else {
   error("Incorrect usage", true)
+}
+
+function processFile(content){
+  content = content.toUpperCase()
+  console.log(content);
 }
 
 function printHelp() {
   console.log("index usage:");
+  console.log(" index.js --file={FILENAME}");
   console.log("");
   console.log("--help          print the help");
+  console.log("--file          file to process");
   console.log("");
 }
-console.log(args);
 
 function error(msg, includeHelp = false){
   console.error(msg);
