8271b31575ff0a7c9c70d04ee0d4fa61f214f500
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2025-03-08 22:05:34 +0400

    added stdin into project to get file from terminal

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 #!/usr/bin/env node
 import path from "path";
 import fs from "fs";
+import getStdin from "get-stdin";
 import minimist from "minimist";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const args = minimist(process.argv.slice(2), {
   boolean: ["help"],
   string: ["file"],
 });
 
+const BASE_PATH = path.resolve(process.env.BASE_PATH || __dirname)
+
 if (args.help) {
   printHelp();
+} else if (args.in) {
+  getStdin().then(processFile).catch(error)
 } else if (args.file) {
-  fs.readFile(path.resolve(args.file), "utf-8", function onContents(err, contents) {
+  fs.readFile(path.join(BASE_PATH, args.file), "utf-8", function onContents(err, contents) {
     if(err){
       error(err.toString(), true)
     } else {
